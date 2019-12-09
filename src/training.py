2472e19c87eb53b76288ba1b1b2637fee8dd1941
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-12-08 21:07:01 -0500

    fix args read 2

--- src/training.py
+++ src/training.py
@@ -53,8 +53,8 @@ def main():
         for l in cf:
             config[l.split(':')[0]] = l.split(':')[1].rstrip()
     if mode == 'v':
-        build_vocab(sent_file, config['vocab_file'], int(config['vocab_size'],
-        int(config['subsample_size']), int(config['num_placeholders'])))
+        build_vocab(sent_file, config['vocab_file'], int(config['vocab_size']),
+        int(config['subsample_size']), int(config['num_placeholders']))
 
 if __name__ == '__main__':
     main()
